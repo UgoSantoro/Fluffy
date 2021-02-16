44ef2a5379b2c4ef5553f3492f3e8228b0392021
Ugo Santoro <ugo.santoro@epitech.eu>
2021-02-16 15:17:43 +0100

    Add Login Menu

--- fluffy/lib/main.dart
+++ fluffy/lib/main.dart
@@ -33,7 +33,11 @@ class MyApp extends StatelessWidget {
         '/login': (BuildContext context) => new LoginPage(),
         '/home': (BuildContext context) => new MyHomePage(),
         '/detail': (BuildContext context) => new DetailPage()
-        //'/setting': (BuildContext context) => new Screen4()
+        //'/Facebook': (BuildContext context) => new Screen4()
+        //'/Twitter': (BuildContext context) => new Screen4()
+        //'/Linkedln': (BuildContext context) => new Screen4()
+        //'/Instagram': (BuildContext context) => new Screen4()
+        //'/Settings': (BuildContext context) => new SettingPage()
       },
     );
   }
@@ -167,19 +171,95 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('This is a login page'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Open route'),
-          onPressed: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(builder: (context) => MyHomePage()),
-            );
-          },
-        ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: FractionallySizedBox(
+              heightFactor: 0.8,
+              child: Container(
+                color: Color.fromRGBO(86, 0, 232, 1),
+                child:
+                    Image.asset('assets/images/icon.png', fit: BoxFit.contain),
+              ),
+            ),
+          ),
+          Container(
+            height: 40,
+            width: 200,
+            child: ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => MyHomePage()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+                primary: Color.fromRGBO(25, 118, 210, 1),
+              ),
+              icon: Icon(Icons.home),
+              label: Text("Facebook"),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromRGBO(254, 218, 117, 1),
+                  Color.fromRGBO(250, 126, 30, 1),
+                  Color.fromRGBO(214, 41, 118, 1),
+                  Color.fromRGBO(150, 47, 191, 1),
+                  Color.fromRGBO(79, 91, 213, 1),
+                ],
+                begin: FractionalOffset.centerLeft,
+                end: FractionalOffset.centerRight,
+              ),
+            ),
+            child: ElevatedButton.icon(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: new RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0),
+                  ),
+                  primary: Color.fromRGBO(0, 0, 0, 0),
+                ),
+                icon: Icon(Icons.home),
+                label: Text("Instagram")),
+          ),
+          Container(
+            height: 40,
+            width: 200,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+                primary: Color.fromRGBO(86, 0, 232, 1),
+              ),
+              icon: Icon(Icons.home),
+              label: Text("Twitter"),
+            ),
+          ),
+          Container(
+            height: 40,
+            width: 200,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: new RoundedRectangleBorder(
+                  borderRadius: new BorderRadius.circular(30.0),
+                ),
+                primary: Color.fromRGBO(86, 0, 232, 1),
+              ),
+              icon: Icon(Icons.home),
+              label: Text('LinkedIn'),
+            ),
+          ),
+        ],
       ),
     );
   }
