aa6ebe03959571f152641d49606d0e0ac73b3560
Ugo Santoro <ugo.santoro@epitech.eu>
2021-03-22 10:49:27 +0100

    Edit Twitter & Facebook Post Infos

--- fluffy/lib/Model/Example_Data/example_data.dart
+++ fluffy/lib/Model/Example_Data/example_data.dart
@@ -12,12 +12,12 @@ class Example_data extends StatelessWidget {
     return ListView(
       children: <Widget>[
         Friends_TMP(
-                nameuser: Text("Test"),
+                nameuser: Text("Billy Mitchel"),
                 userimage: Text(
-                    "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"))
+                    "https://image.jeuxvideo.com/medias-md/159258/1592580329-9447-card.png"))
             .buildFriends_template(),
         Friends_TMP(
-                nameuser: Text("Username"),
+                nameuser: Text("CampanBernard"),
                 userimage: Text(
                     "https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg"))
             .buildFriends_template(),
@@ -30,12 +30,12 @@ class Example_data extends StatelessWidget {
     return ListView(
       children: <Widget>[
         Subs_TMP(
-                nameuser: Text("dragon-ball"),
+                nameuser: Text("Eric Lioacono"),
                 userimage: Text(
-                    "https://www.journaldugeek.com/content/uploads/2021/01/dragon-ball-4995675-1280-640x492.jpg"))
+                    "https://media-exp1.licdn.com/dms/image/C4E03AQGPo1rqDYUPlQ/profile-displayphoto-shrink_200_200/0/1552574596149?e=1617235200&v=beta&t=TpwjiFCzyYPwNfihK-9Y2BOe80d-g9wGpw1aBegMuQ8"))
             .buildFriends_template(),
         Subs_TMP(
-                nameuser: Text("Sonic"),
+                nameuser: Text("Marco"),
                 userimage: Text(
                     "https://www.journaldugeek.com/content/uploads/2021/01/dragon-ball-4995675-1280-640x492.jpg"))
             .buildFriends_template(),
@@ -49,31 +49,28 @@ class Example_data extends StatelessWidget {
       children: <Widget>[
         Posts_TMP().build_posts_template(
             context,
-            Text("username"),
+            Text("Jean"),
             Text(
-                "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"),
-            Text(
-                "dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It"),
+                "https://media.senscritique.com/media/000007682248/source_big/French_Food_Porn.png"),
+            Text("Yummy Breakfast"),
             [
               Text(
-                  "https://static.vecteezy.com/ti/vecteur-libre/p3/246312-mountain-lake-sunset-landscape-vue-a-la-premiere-personne-gratuit-vectoriel.jpg"),
+                  "https://media-cdn.tripadvisor.com/media/photo-s/17/57/7d/17/2-egg-breakfast.jpg"),
               Text(
-                  "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
+                  "https://www.food2vous.com/wp-content/uploads/2018/05/Agence-foodporn-communication.jpg"),
               Text(
-                  "https://www.lense.fr/wp-content/uploads/2020/12/International-Landscape-Photographer-Year-PhotographER-1st-KelvinYuen-2.jpg"),
+                  "https://www.audreycuisine.fr/wp-content/uploads/2020/10/20201008_163700-1.jpg"),
+              Text(
+                  "https://media-cdn.tripadvisor.com/media/photo-s/16/85/8d/cb/the-breakfast-club.jpg"),
             ],
             Text("215"),
             Text("87"),
             Text("10"),
             [
-              Comment("bernard", "woaw",
-                  "https://i.pinimg.com/originals/95/c4/65/95c4654db07456e6b9a3640253cd76a9.jpg"),
-              Comment("Elon", "such beautiful view",
-                  "https://www.presse-citron.net/app/uploads/2019/10/elonmusk-e1611101293534.jpg"),
-              Comment("Gaby", "must be cold to be there...",
-                  "https://www.nautiljon.com/images/perso/00/22/gaby_braun_19822.jpg"),
-              Comment("Zavala", "Next Vacantion for me x)",
-                  "https://media.altchar.com/prod/images/940_530/gm-6ddd714a-81e2-49e0-b2fa-1feac00c4095-zavala.JPG"),
+              Comment("Francois", "Best Food Pictures",
+                  "https://www.babelio.com/users/AVT_Francois-Theurel_4092.jpg"),
+              Comment("Patrick", "fascinating...!",
+                  "https://pbs.twimg.com/profile_images/1227251664496676864/n0U2WsDk_400x400.jpg"),
             ]),
       ],
     );
@@ -187,22 +184,22 @@ class Example_data extends StatelessWidget {
     return ListView(
       children: <Widget>[
         Friends_TMP(
-                nameuser: Text("Test"),
+                nameuser: Text("Durendal"),
                 userimage: Text(
                     "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"))
             .buildFriends_template(),
         Friends_TMP(
-                nameuser: Text("Username"),
+                nameuser: Text("Dylan"),
                 userimage: Text(
                     "https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg"))
             .buildFriends_template(),
         Friends_TMP(
-                nameuser: Text("Sonic"),
+                nameuser: Text("Benoit"),
                 userimage: Text(
                     "https://www.journaldugeek.com/content/uploads/2021/01/dragon-ball-4995675-1280-640x492.jpg"))
             .buildFriends_template(),
         Friends_TMP(
-                nameuser: Text("Earth"),
+                nameuser: Text("Quentin"),
                 userimage: Text(
                     "https://img.huffingtonpost.com/asset/5d724df83b0000683fd0abe1.png?cache=pokxgDhxmj&ops=scalefit_630_noupscale"))
             .buildFriends_template(),
@@ -215,19 +212,14 @@ class Example_data extends StatelessWidget {
     return ListView(
       children: <Widget>[
         Subs_TMP(
-                nameuser: Text("Username"),
+                nameuser: Text("Benzaie"),
                 userimage: Text(
-                    "https://img-19.ccm2.net/ppaPB1I48R0LInb9Z8QBoUqXqSQ=/480x335/smart/b829396acc244fd484c5ddcdcb2b08f3/ccmcms-commentcamarche/20494859.jpg"))
+                    "https://static1-fr.millenium.gg/article_old/images/contenu/actus/CoD/top_5_ghosts/benzounet.jpg"))
             .buildFriends_template(),
         Subs_TMP(
-                nameuser: Text("dragon-ball"),
+                nameuser: Text("Alt"),
                 userimage: Text(
-                    "https://www.journaldugeek.com/content/uploads/2021/01/dragon-ball-4995675-1280-640x492.jpg"))
-            .buildFriends_template(),
-        Subs_TMP(
-                nameuser: Text("Sonic"),
-                userimage: Text(
-                    "https://www.journaldugeek.com/content/uploads/2021/01/dragon-ball-4995675-1280-640x492.jpg"))
+                    "https://yt3.ggpht.com/ytc/AAUvwnjh732RmQ3asOvYtJoxUuliVGkki5SpdZiQbML2aw=s900-c-k-c0x00ffffff-no-rj"))
             .buildFriends_template(),
       ],
     );
@@ -239,58 +231,46 @@ class Example_data extends StatelessWidget {
       children: <Widget>[
         Posts_TMP().build_posts_template(
             context,
-            Text("y,yu,"),
+            Text("Durendal"),
             Text(
                 "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"),
-            Text("fdfhbthnjtrnytn,yhnfgndfn"),
+            Text("Some of ours best film set !!!"),
             [
+              Text("https://media.routard.com/image/05/6/skyfall.1374056.jpg"),
               Text(
-                  "https://static.vecteezy.com/ti/vecteur-libre/p3/246312-mountain-lake-sunset-landscape-vue-a-la-premiere-personne-gratuit-vectoriel.jpg"),
+                  "http://winnetou.fr/highslide/images/lieux_mythiques/wadi_rum_full.jpg"),
               Text(
-                  "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-              Text(
-                  "https://www.lense.fr/wp-content/uploads/2020/12/International-Landscape-Photographer-Year-PhotographER-1st-KelvinYuen-2.jpg"),
+                  "https://tour-monde.fr/wp-content/uploads/2013/07/8-maison-troglodyte-tunisie.jpg"),
             ],
             Text("215"),
             Text("87"),
             Text("10"),
             [
-              Comment("bernard", "woaw",
+              Comment("Bernard", "WOOOOAAAAWWWW",
                   "https://i.pinimg.com/originals/95/c4/65/95c4654db07456e6b9a3640253cd76a9.jpg"),
-              Comment("Elon", "crazy!!!",
+              Comment("Elon", "So wonderfull <3",
                   "https://www.presse-citron.net/app/uploads/2019/10/elonmusk-e1611101293534.jpg"),
-              Comment("Gaby", "must be cold to be there...",
-                  "https://www.nautiljon.com/images/perso/00/22/gaby_braun_19822.jpg"),
               Comment("Zavala", "Next Vacantion for me x)",
                   "https://media.altchar.com/prod/images/940_530/gm-6ddd714a-81e2-49e0-b2fa-1feac00c4095-zavala.JPG"),
             ]),
         Posts_TMP().build_posts_template(
             context,
-            Text("username"),
+            Text("Durendal"),
             Text(
                 "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"),
-            Text(
-                "fdbgh;iumoàmyhrhrergehrrgrggergergergergregregregregregrgrgregregn rtgregregreregergrere"),
+            Text("memories....memories....memories...."),
             [
               Text(
-                  "https://static.vecteezy.com/ti/vecteur-libre/p3/246312-mountain-lake-sunset-landscape-vue-a-la-premiere-personne-gratuit-vectoriel.jpg"),
-              Text(
-                  "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"),
-              Text(
-                  "https://www.lense.fr/wp-content/uploads/2020/12/International-Landscape-Photographer-Year-PhotographER-1st-KelvinYuen-2.jpg"),
+                  "https://www.jeuneafrique.com/medias/2017/07/17/tunisie-by-pixabay-592x296.jpg"),
             ],
             Text("25"),
             Text("9"),
             Text("10"),
             [
-              Comment("bernard", "woaw",
-                  "https://i.pinimg.com/originals/95/c4/65/95c4654db07456e6b9a3640253cd76a9.jpg"),
-              Comment("Elon", "crazy!!!",
-                  "https://www.presse-citron.net/app/uploads/2019/10/elonmusk-e1611101293534.jpg"),
-              Comment("Gaby", "must be cold to be there...",
-                  "https://www.nautiljon.com/images/perso/00/22/gaby_braun_19822.jpg"),
-              Comment("Zavala", "Next Vacantion for me x)",
-                  "https://media.altchar.com/prod/images/940_530/gm-6ddd714a-81e2-49e0-b2fa-1feac00c4095-zavala.JPG"),
+              Comment("Benoit", "This is beautiful, where is it ?",
+                  "https://scontent-cdg2-1.xx.fbcdn.net/v/t1.0-9/15965282_1119234888198439_3251091215350487655_n.jpg?_nc_cat=108&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=vtU9QvQZNPcAX8jTDrB&_nc_ht=scontent-cdg2-1.xx&oh=da7e1a25efbe44de1a1bbc23b8add355&oe=607F4A90"),
+              Comment("Durendal", "It is the cafe des delices in Tunisia",
+                  "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"),
             ]),
       ],
     );
